d905e4805c03815b579663dafe0a69c2547c7d64
Ritik <ritik.sharma@taazaa.com>
2021-03-28 18:05:43 +0530

    Event

---
+++ C-Sharp/Lab 10/Demo 1/demo/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace demo
+{
+    public class Manager
+    {
+        Agent a;
+        public Manager()
+        {
+            a = new Agent();
+        }
+        public void manage() {
+            Console.WriteLine("Task Completed Send Message Now ->");
+            a.sendMessage(CallType.CallByPhone);
+        }
+    }
+}
---
+++ C-Sharp/Lab 10/Demo 1/demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Manager m = new Manager();
+            m.manage();
+        }
+    }
+}
