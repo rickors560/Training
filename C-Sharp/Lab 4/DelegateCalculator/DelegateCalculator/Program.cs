f865f7f3342c7a129d2e9be3de1aa2c18bf3b0c1
Ritik <ritik.sharma@taazaa.com>
2021-03-28 18:02:16 +0530

    Delegate

---
+++ C-Sharp/Lab 4/DelegateCalculator/DelegateCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DelegateCalculator
+{
+    class Program
+    {   
+        public delegate void CalDelegate(int a,int b);
+        static void Main(string[] args)
+        {
+            CalDelegate d = new CalDelegate(Add);
+            d(10, 5);
+
+            d = Sub;
+            d(10, 5);
+            
+            d = Mul;
+            d(10, 5);
+            
+            d = Div;
+            d(10, 5);
+        }
+        static void Add(int a, int b) {
+            Console.WriteLine($"{a} + {b} = {a+b}");
+        }
+        static void Sub(int a, int b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+        static void Mul(int a, int b)
+        {
+            Console.WriteLine($"{a} * {b} = {a * b}");
+        }
+        static void Div(int a, int b)
+        {
+            Console.WriteLine($"{a} / {b} = {a / b}");
+        }
+    }
+}
