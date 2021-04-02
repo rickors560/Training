a7d64ab99404765c1126497db1de34dae959fdd5
Ritik Sharma <ritik.sharma@taazaa.com>
2021-04-02 18:11:29 +0530

    Angular

--- Angular/AgularDemo/src/app/purchaseorder/purchaseorder.component.ts
+++ Angular/AgularDemo/src/app/purchaseorder/purchaseorder.component.ts
@@ -7,7 +7,7 @@ import { IPurchaseItem } from '../purchase/IPurchaseItem';
   templateUrl: './purchaseorder.component.html',
   styleUrls: ['./purchaseorder.component.css']
 })
-export class PurchaseorderComponent implements OnInit , OnChanges, DoCheck {
+export class PurchaseorderComponent implements OnInit, OnChanges{
 
   constructor() { }
   ngOnInit(): void {
@@ -21,20 +21,20 @@ export class PurchaseorderComponent implements OnInit , OnChanges, DoCheck {
        this.price = this.price + i.Price*i.Quantity;
      });
   }
-  ngDoCheck(){
-    this.price = 0;
-    this.productdetails.Items.forEach(i => {
-      if(i.Price == null){
-        i.Price = 0;
-      }
-      if(i.Quantity == null){
-        i.Quantity = 0;
-      }
-      this.price = this.price + i.Price*i.Quantity;
-    });
-  }
   sendProductData():void{
     
     this.sendProductDataEvent.emit(this.productdetails.Purchase_ID);
+  }
+  calculatePrice():void{
+    this.price = 0;
+    this.productdetails.Items.forEach(i => {
+      if(i.Price < 0 || i.Price == null){
+        i.Price =0;
+      }
+      if(i.Quantity < 0 || i.Quantity == null){
+        i.Quantity =0;
+      }
+      this.price = this.price + i.Price*i.Quantity;
+    });
   }
 }
