e8317c77e76da6a8d87c13c666494f872864d61d
Ritik <ritik.sharma@taazaa.com>
2021-04-08 19:58:37 +0530

    DB1stApproach

---
+++ C-Sharp/Data1stApproach/Data1stApproach/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data1stApproach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DB1stApproachEntities();
+
+            //var bat = new Product {
+            //    Id = 3,
+            //    Title = "Bat",
+            //    Price = 1000
+            //};
+            //context.Products.Add(bat);
+
+            //var findBat = context.Products.Find(3);  //cant remove a item directly we have to find it.
+            //context.Products.Remove(findBat);
+
+            //var findBat = context.Products.Find(3);
+            //findBat.Price = 1500;
+
+            //context.SaveChanges();
+            var products = context.Products.Select((i)=> i);
+            products.ToList().ForEach(
+                (p) => {
+                    Console.WriteLine(p.ToString()); 
+                }
+                );
+        }
+    }
+}
